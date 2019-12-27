5c2d9b745e47454f2562ed36424687f49d08b099
spulakk <krystof.spulak@modulservis.cz>
2019-12-27 14:57:39 +0100

    add test file

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test;
+
+const A = 'A';
+const B = 'B';
+
+class Test
+{
+    public function test1()
+    {
+        echo A;
+    }
+
+    public function test2()
+    {
+        echo B;
+    }
+}
