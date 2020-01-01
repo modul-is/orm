025734aed85041db2068e9636bacf159f08a48a2
Pavel Kravčík <kravcik.pavel@gmail.com>
2020-01-01 20:59:43 +0100

    add class map

--- src/classmap.php
+++ src/classmap.php
@@ -9,3 +9,4 @@ require_once __DIR__ . '/ModulIS/Repository.php';
 require_once __DIR__ . '/ModulIS/EntityCollection.php';
 require_once __DIR__ . '/ModulIS/Record.php';
 require_once __DIR__ . '/ModulIS/Entity.php';
+require_once __DIR__ . '/ModulIS/MySqlDriver.php';
